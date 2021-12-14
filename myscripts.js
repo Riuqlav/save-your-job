0a91f88c96ada2d35a6f9743aefbbaf6653998fd
Riuqlav <valquirsolza.vs@gmail.com>
2021-12-14 10:23:19 +0100

    Variables accepted in the game

--- myscripts.js
+++ myscripts.js
@@ -1,5 +1,22 @@
-//function that generates an alert with prompt
+//Variables accepted in the game
 
-//declaration of game choices
+let R = "r" && "✊" && "rock" && "Rock";
+let P = "p" && "🖐" && "paper" && "Paper";
+let S = "s" && "✌️" && "scissors" && "Scissors";
 
-//Function random assignment of one of the 3 choices
+//function computerPlay with random option
+function computerChoice() {
+  const randomPick = [R, P, S];
+  const randomNumber = Math.floor(Math.random() * 3);
+  return randomPick[randomNumber];
+}
+
+//function that generates an alert with prompt asking for R,S,P with timer.
+function promptGame() {
+  prompt("Lets play a game, what do you pick? Rock, Paper or Scissors?");
+}
+promptGame();
+
+//compare random option of the computer above with user entry.
+
+//return another alert with display of random option, and  winner or looser or tie
