cdb83fd9eff51c49ef47136da971c07ca795f271
Riuqlav <valquirsolza.vs@gmail.com>
2021-12-15 20:28:37 +0100

    promptGame, computerPlay and Variables fixed

--- myscripts.js
+++ myscripts.js
@@ -1,30 +1,27 @@
 //Variables accepted in the game
 
-let R = "r" && "✊" && "rock" && "Rock";
-let P = "p" && "🖐" && "paper" && "Paper";
-let S = "s" && "✌️" && "scissors" && "Scissors";
+let R = ["r", "✊", "rock", "Rock"];
+let P = ["p", "🖐", "paper", "Paper"];
+let S = ["s", "✌️", "scissors", "Scissors"];
 
 //function computerPlay with random option
 function computerChoice() {
   const randomPick = [R, P, S];
   const randomNumber = Math.floor(Math.random() * 3);
-  return randomPick[randomNumber];
+  return randomPick[randomNumber][0];
 }
 
 //function that generates an alert with prompt asking for R,S,P, then one for "are you sure?"
 
 function promptGame() {
-  u = prompt("Lets play a game, what do you pick? Rock, Paper or Scissors?");
-  if (u == R || u == S || u == P) {
+  u = window.prompt("Let's play a game!", "Choose Rock, Paper or Scissors.");
+  if (R.includes(u) || P.includes(u) || S.includes(u)) {
     grabWinner();
-  } else if (alert("Come on man, you know the game, let's do it"));
-  alert("are you sure?");
+  } else if (promptGame());
 }
-console.log(promptGame());
 
 //User or computer or tie, function to grabWinner
-
-function grabWinner() {
+function grabWinner(u) {
   if (R.includes(u) && computerChoice() === P) {
     alert(computerChoice() + " I won this time, you're terrible at this game");
   } else if (R.includes(u) && computerChoice() === S) {
