6e0195ee578f485423b4fedbb8e239213d276c6e
Riuqlav <valquirsolza.vs@gmail.com>
2021-12-15 12:12:18 +0100

    Rules of the game fixed

--- myscripts.js
+++ myscripts.js
@@ -14,28 +14,37 @@ function computerChoice() {
 
 
 
-//User or computer or tie function grabWinner {
-    function grabWinner() {
-        if      (u === R && computerChoice() === P) {
-        alert (computerChoice() + " I won this time, you suck at this game"   )}
-        else if (u === R && computerChoice() === S) {
-        alert (computerChoice() + " Ok you won, let's play again" )}
-        else if (u === R && computerChoice() === R) {
-        alert (computerChoice() + " Tie, let's play again"  )} 
-        else if (u === P && computerChoice() === P) {
-        alert (computerChoice() + " Tie, let's play again")} 
-        else if (u === P && computerChoice() === S) {
-        alert (computerChoice() + " I won this time, you suck at this game"        )} 
-        else if (u === P && computerChoice() === R) {
-        alert (computerChoice() + " Ok you won, let's play again"   )} 
-        else if (u === S && computerChoice() === P) {
-        alert (computerChoice() + " Ok you won, let's play again"    )} 
-        else if (u === S && computerChoice() === S) {
-        alert (computerChoice() + " Tie, let's play again"        )} 
-        else if (u === S && computerChoice() === R);
-        alert (computerChoice() + " I won this time, you suck at this game"        )
-      }
 
+
+
+
+//User or computer or tie function grabWinner {
+  function grabWinner() {
+    if      (u === R && computerChoice() === P) {
+    alert (computerChoice() + " I won this time, you suck at this game")}
+    else if (u === R && computerChoice() === S) {
+    alert (computerChoice() + " Ok you won, let's play again")}
+    else if (u === R && computerChoice() === R) {
+    alert (computerChoice() + " Tie, let's play again")} 
+    else if (u === P && computerChoice() === P) {
+    alert (computerChoice() + " Tie, let's play again")} 
+    else if (u === P && computerChoice() === S) {
+    alert (computerChoice() + " I won this time, you suck at this game")} 
+    else if (u === P && computerChoice() === R) {
+    alert (computerChoice() + " Ok you won, let's play again")} 
+    else if (u === S && computerChoice() === P) {
+    alert (computerChoice() + " Ok you won, let's play again")} 
+    else if (u === S && computerChoice() === S) {
+    alert (computerChoice() + " Tie, let's play again")} 
+    else if (u === S && computerChoice() === R)
+    alert (computerChoice() + " I won this time, you suck at this game")
+    else if (u != S, R, P){
+    alert ("Come on, you know how to play the game")
+    grabWinner()}
+  };
+
+
+  
 //function that generates an alert with prompt asking for R,S,P
 function promptGame() {
   let u = prompt(
@@ -46,6 +55,4 @@ function promptGame() {
   } else if (alert("Come on man, you know the game, let's do it"));
   alert("are you sure?")
 };
-function promptGame()
-
-//return another alert with display of random option, and  winner or looser or tie;*/
+function promptGame();
